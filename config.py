ecc1eaab53307274af476cb44e65539645a29dd1
Yadi <yadimeng@yahoo.com>
2018-06-08 17:46:13 -0500

    News Mood analysis from Media Tweets

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "gkX5USuMlamnhkjLFlHhyAdvg"
+consumer_secret_key = "Y5spfBdv0ow4SPi7wdGxISDQCcoUYnbhCVJBXNrn7rrDA26xz6"
+access_token = "999795077810601984-PF9fgSvWXG8y2LYSZbbUT19IvGpg8LV"
+access_token_secret = "2545egw5FwOyqwmUw7M1slzOWQEvcPiR15uWIsvhOTDH9"
